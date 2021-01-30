99362ac124ccf3bf9d8f3734035aef1922910795
Adeesha Wijesinghe <63795833+anw98@users.noreply.github.com>
2021-01-30 21:28:25 +0530

    Add files via upload

---
+++ application/View/customer/account.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+    <head>
+        <title>Account</title>
+        <meta charset ="UTF-8">
+        <meta name="viewpoint" content="width-device-width initial-scale=1.0">
+        <h1>Account Information</h1> 
+
+    </head>
+    <body>
+        <table>
+            <link rel="stylesheet" href="../../../public/css/table.css">  
+                <tr>
+                    <td>Name</td> <td> <input type="text" style="width: 100%"> </td>
+                </tr>
+                <tr>
+                    <td>Email</td> <td> <input type="text" style="width: 100%"></td>
+                </tr>
+        
+                <tr>
+                    <td>Address</td> <td><input type="text" style="width: 100%"> </td>
+                </tr>
+                <tr>
+                    <td>Mobile Number</td> <td> <input type="text" style="width: 100%"></td>
+                </tr>
+                <tr>
+                    <td>Card Details</td> <td> <input type="text" style="width: 100%"></td>
+                </tr>
+                
+</table>
+<br>
+<br><center><input type="submit" class="link" name="Save" style="margin-bottom: 50px; " value="Save"></center>
+
+              
+            
+                
+              
+</body>
+</html>
+        
+        
---
+++ application/View/customer/order_status.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+    <head>
+        <title>Order Status</title>
+        <meta charset ="UTF-8">
+        <meta name="viewpoint" content="width-device-width initial-scale=1.0">
+       
+        <h1 align='center'>Order Status</h1>
+
+    </head>
+    <body>
+        <table width="80%">
+        <link href="../../../public/css/table.css" rel="stylesheet" />
+           
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Order ID</th>
+                    <th>Order</th>
+                    <th>Total</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+        
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+        
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                   
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
