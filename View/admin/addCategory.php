2ca17cdaaab5eaf837b2018065323ebc515ea255
Jamini <jamini.18@cse.mrt.ac.lk>
2021-01-30 15:55:38 +0530

    View folder

---
+++ View/admin/addCategory.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<head>
+<html lang="en">
+<meta charset="utf-8">
+<title>Update the Product</title>
+</head>
+
+<body>
+<div class= container>
+    <h1>Add/Remove Category</h1>
+    <table>
+    <!-- <img src="https://images.search.yahoo.com/search/images;_ylt=Awr9ImrnLBVg8akAMIBXNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZANBMDYxNV8xBHNlYwNzYw--?p=headphone+image&fr=mcafee&th=102.5&tw=102.5&imgurl=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1YlKISpXXXXa_XXXXq6xXFXXXY%2FOriginal-Kotion-EACH-G2000-Computer-Stereo-Gaming-Headphones-Deep-Bass-Game-Earphone-Headset-with-Mic-LED.jpg&rurl=https%3A%2F%2Fwww.aliexpress.com%2Fitem%2FOriginal-Kotion-EACH-G2000-Computer-Stereo-Gaming-Headphones-Deep-Bass-Game-Earphone-Headset-with-Mic-LED%2F32669950071.html&size=117KB&name=Original+Kotion+EACH+G2000+Computer+Stereo+Gaming+Headphones+Deep+Bass+Game+Earphone+Headset+...&oid=1&h=1000&w=1000&turl=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OmHnpLY4CB1LUgMoGFLW8wHaHa%26amp%3Bpid%3DApi%26rs%3D1%26c%3D1%26qlt%3D95%26w%3D102%26h%3D102&tt=Original+Kotion+EACH+G2000+Computer+Stereo+Gaming+Headphones+Deep+Bass+Game+Earphone+Headset+...&sigr=h1QdCLkGC4t2&sigit=uoaqNmb8cNZb&sigi=xXPPMcg0INEM&sign=iTQdTlUYjUej&sigt=iTQdTlUYjUej"> -->
+    <tr>
+    <td>
+        <label for="category">Category:</label><br><br>
+        <input type="text" id="category" name="category"><br><br>
+        <input type="submit" class="link" name="search" style="margin-bottom: 50px;" id="search" value="search">
+
+    </td>
+    </tr>
+    <tr>
+    <td>
+        <label for="sub_category1">Sub Category:</label><br><br>
+        <input type="text" id="sub_category1" name="sub_category1"><br><br>
+    </td>
+    <td>
+        <label for="sub_category2">Sub Category:</label><br><br>
+        <input type="text" id="sub_category2" name="sub_category2"><br><br>
+    </td>
+    </tr>
+</table>
+
+<input type="submit" class="link" name="update" style="margin-bottom: 50px;" id="update" value="Update">
+<input type="submit" class="link" name="Remove" style="margin-bottom: 50px;" id="Remove" value="Remove">
+
+
+</div>
+</body>
+</html>
---
+++ View/admin/product_update.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<head>
+<html lang="en">
+<meta charset="utf-8">
+<title>Update the Product</title>
+</head>
+
+<body>
+<div class= container>
+    <h1>Update the product</h1>
+    <table>
+    <!-- <img src="https://images.search.yahoo.com/search/images;_ylt=Awr9ImrnLBVg8akAMIBXNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZANBMDYxNV8xBHNlYwNzYw--?p=headphone+image&fr=mcafee&th=102.5&tw=102.5&imgurl=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1YlKISpXXXXa_XXXXq6xXFXXXY%2FOriginal-Kotion-EACH-G2000-Computer-Stereo-Gaming-Headphones-Deep-Bass-Game-Earphone-Headset-with-Mic-LED.jpg&rurl=https%3A%2F%2Fwww.aliexpress.com%2Fitem%2FOriginal-Kotion-EACH-G2000-Computer-Stereo-Gaming-Headphones-Deep-Bass-Game-Earphone-Headset-with-Mic-LED%2F32669950071.html&size=117KB&name=Original+Kotion+EACH+G2000+Computer+Stereo+Gaming+Headphones+Deep+Bass+Game+Earphone+Headset+...&oid=1&h=1000&w=1000&turl=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OmHnpLY4CB1LUgMoGFLW8wHaHa%26amp%3Bpid%3DApi%26rs%3D1%26c%3D1%26qlt%3D95%26w%3D102%26h%3D102&tt=Original+Kotion+EACH+G2000+Computer+Stereo+Gaming+Headphones+Deep+Bass+Game+Earphone+Headset+...&sigr=h1QdCLkGC4t2&sigit=uoaqNmb8cNZb&sigi=xXPPMcg0INEM&sign=iTQdTlUYjUej&sigt=iTQdTlUYjUej"> -->
+    <tr>
+    <td>
+        <label for="pname">Product name:</label><br><br>
+        <input type="text" id="pname" name="pname"><br><br>
+        <input type="submit" class="link" name="search" style="margin-bottom: 50px;" id="search" value="search">
+
+    </td>
+    </tr>
+
+    <tr>
+    <td>
+        <label for="price">Price:</label><br><br>
+        <input type="number" step="0.01" id="price" name="price"><br><br>
+    </td>
+
+    <td>
+        <label for="color">Color:</label><br><br>
+        <input type="text" id="color" name="color"><br><br>
+    </td>
+    </tr>
+
+    <tr>
+    <td>
+        <label for="brand">Brand:</label><br><br>
+        <input type="text" id="brand" name="brand"><br><br>
+    </td>
+
+    <td>
+        <label for="P_dimension">Product Dimensions:</label><br><br>
+        <input type="text" id="P_dimension" name="P_dimension"><br><br>
+    </td>
+    </tr>
+
+    <tr>
+    <td>
+        <label for="p_weight">Product Weight(grams):</label><br><br>
+        <input type="number" step="any" id="p_weight" name="p_weight"><br><br>
+    </td>
+
+    <td>
+        <label for="sku">SKU:</label><br><br>
+        <input type="text" id="sku" name="sku"><br><br>
+    </td>
+    </tr>
+
+    <tr>
+    <td>
+        <label for="category">Category:</label><br><br>
+        <input type="text" id="category" name="category"><br><br>
+    </td>
+
+    <td>
+        <label for="sub_category">Sub Category:</label><br><br>
+        <input type="text" id="sub_category" name="sub_category"><br><br>
+    </td>
+    </tr>
+    <tr>
+    <td>
+        <label for="about">About this product:</label><br><br>
+        <input type="text" style="width:250px;height:90px;" id="about" name="about"><br><br>
+    </td>
+    <tr>
+</table>
+
+<input type="submit" class="link" name="update" style="margin-bottom: 50px;" id="update" value="Update">
+<input type="submit" class="link" name="Remove" style="margin-bottom: 50px;" id="Remove" value="Remove">
+
+
+</div>
+</body>
+</html>
