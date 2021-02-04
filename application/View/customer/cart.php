00a1ad707415ec46e0a588c0a434a41875f666f8
Supuni-Jayasinghe <63720402+Supuni-Jayasinghe@users.noreply.github.com>
2021-02-04 12:20:51 +0530

    Update cart.php

--- application/View/customer/cart.php
+++ application/View/customer/cart.php
@@ -5,8 +5,12 @@
 <script src="../../../public/js/jquery-1.11.1.min.js"></script>
 
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="container">
-	<table id="cart" class="table table-hover table-condensed">
+<link rel="stylesheet" href="../../../public/css/login.css" />
+
+<div class="container" style="margin-top:100px;">
+<h1 align='left' margin="100px" ><i><b>Cart</b></i></h1>
+	<table id="cart" class="table table-hover table-condensed" >
+	
     				<thead>
 						<tr>
 							<th style="width:50%">Product</th>
@@ -51,5 +55,6 @@
 					</tfoot>
 				</table>
 </div>
+
 
 </html>
