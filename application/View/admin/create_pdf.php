5259c8fd89182680f508fea47e3b4429acb848f1
Supuni-Jayasinghe <supuni.18@cse.mrt.ac.lk>
2021-02-15 21:02:42 +0530

    admin

--- application/View/admin/create_pdf.php
+++ application/View/admin/create_pdf.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 //create_pdf.php
@@ -19,25 +18,5 @@ if(isset($_POST["hidden_html"]) && $_POST["hidden_html"] != '')
 }
 
 ?>
-=======
-<?php
-
-//create_pdf.php
 
 
-include('pdf.php');
-
-if(isset($_POST["hidden_html"]) && $_POST["hidden_html"] != '')
-{
- $file_name = 'google_chart.pdf';
- $html = '<link rel="stylesheet" href="bootstrap.min.css">';
- $html .= $_POST["hidden_html"];
-
- $pdf = new Pdf();
- $pdf->load_html($html);
- $pdf->render();
- $pdf->stream($file_name, array("Attachment" => false));
-}
-
-?>
->>>>>>> 9d62b866cc7b8b5000bbd2ccec6bdeda996d6417
