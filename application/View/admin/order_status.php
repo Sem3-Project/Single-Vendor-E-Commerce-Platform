98aac804ebc5ef36fb4661cac9102750288ac99f
Adeesha Wijesinghe <63795833+anw98@users.noreply.github.com>
2021-03-01 04:35:34 +0530

    Update order_status.php

--- application/View/admin/order_status.php
+++ application/View/admin/order_status.php
@@ -1,5 +1,39 @@
 <?php
-include '../../model/order_status.model.php';
+//include '../../model/order_status.model.php';
+include '../../controller/order_status_update.class.php';
+
+$connector = new DbConnection();
+$conn = $connector->connect();
+$funObj = new UpdateStatus();
+
+//$order_id='';
+//$delivery_status='';
+//$additional_notes='';
+
+//$update = $funObj->updateStatus($conn,$order_id,$delivery_status,$additional_notes);
+if (isset($_POST['Search'])){
+    $order_id = $_POST['order_id'];  
+    $delivery_status = $_POST['delivery_status'];  
+    $additional_notes = $_POST['additional_notes'];  
+
+    //$update = $funObj->updateStatus($conn,$_POST['order_id'],$_POST['delivery_status'],$_POST['additional_notes']);
+    $query="SELECT delivery_status,additional_notes FROM delivery WHERE order_id = '$order_id'";
+    $query_run=mysqli_query($conn,$query);
+    while($row = mysqli_fetch_array($query_run)){
+    }}
+    
+
+
+if (isset($_POST['Update'])){
+    $order_id = $_POST['order_id'];  
+    $delivery_status = $_POST['delivery_status'];  
+    $additional_notes = $_POST['additional_notes'];  
+
+    //$update = $funObj->updateStatus($conn,$_POST['order_id'],$_POST['delivery_status'],$_POST['additional_notes']);
+    $update="UPDATE delivery SET delivery_status='$delivery_status',additional_notes='$additional_notes' WHERE order_id = '$order_id'";
+}
+//value="<?php echo $row['order_id'] 
+
 ?>
 <!DOCTYPE html>
     <head>
@@ -12,6 +46,10 @@ include '../../model/order_status.model.php';
        
     </head>
     <body>
+    <div class="container">
+	<form action="order_status.php" method="POST">
+		<div class="form-group">
+        <br><br>
         
        
         <h1 align='left' margin="100px" ><i>Update Order Status</i></h1>
@@ -30,24 +68,24 @@ include '../../model/order_status.model.php';
                 </tr>
             </thead>
             <tbody>
+            
             <tr>
-            <td><input type="num" placeholder="Enter Order ID" name="order_id" value="<?=$order_id?>"> </td>
-            <td> <select name="delivery_status">
-            <option value="yes">YES</option>
-            <option value="no">NO</option>
-            <option value="in_transit">In transit</option>
+            <td><input type="num" placeholder="Enter Order ID" name="order_id" >&nbsp;<input type="submit" class="link" name="search" style="margin-bottom: 50px; width:15%; height:40px;background-color:  rgb(236, 185, 17);" value="Search"> </td>
+           
+        <td> <select name="delivery_status">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+            <option value="in_transit">in_transit</option>
             </select> </td>
-            <td><input type="text" placeholder="Add addtional notes" name="additional_notes" value="<?=$additional_notes?>"> </td>
-        
-           
-           
-           
-                
+            <td><input type="text" placeholder="Add addtional notes" name="additional_notes" > </td>
+            
             </tbody>
         </table>
         <br>
         <br><center><input type="submit" class="link" name="update" style="margin-bottom: 50px; width:15%; height:40px;background-color:  rgb(236, 185, 17);" value="Update"></center>
-        
+        </div>
+        </form>
+        </div>
     </body>
    
 </html>
