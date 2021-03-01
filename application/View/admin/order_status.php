9f53d98d32d5bf88aee920699c50dd13eab1282e
Adeesha Wijesinghe <63795833+anw98@users.noreply.github.com>
2021-03-01 08:29:18 +0530

    Update order_status.php

--- application/View/admin/order_status.php
+++ application/View/admin/order_status.php
@@ -10,17 +10,12 @@ $funObj = new UpdateStatus();
 //$delivery_status='';
 //$additional_notes='';
 
-//$update = $funObj->updateStatus($conn,$order_id,$delivery_status,$additional_notes);
 if (isset($_POST['Search'])){
     $order_id = $_POST['order_id'];  
-    $delivery_status = $_POST['delivery_status'];  
-    $additional_notes = $_POST['additional_notes'];  
-
-    //$update = $funObj->updateStatus($conn,$_POST['order_id'],$_POST['delivery_status'],$_POST['additional_notes']);
+    
     $query="SELECT delivery_status,additional_notes FROM delivery WHERE order_id = '$order_id'";
     $query_run=mysqli_query($conn,$query);
-    while($row = mysqli_fetch_array($query_run)){
-    }}
+   }
     
 
 
@@ -46,46 +41,48 @@ if (isset($_POST['Update'])){
        
     </head>
     <body>
-    <div class="container">
-	<form action="order_status.php" method="POST">
-		<div class="form-group">
+    
         <br><br>
         
        
         <h1 align='left' margin="100px" ><i>Update Order Status</i></h1>
         <br><br>
+        &nbsp;&nbsp;&nbsp;<input type="num" placeholder="Enter Order ID" name="order_id" >&nbsp;<input type="submit" class="link" name="Search" style="margin-bottom: 30px; width:10%; height:30px;background-color:  rgb(236, 185, 17);" value="Search"> 
            
-        <table width="80%">
+         <br>  
+         <form action="" method="POST">
+        <table width="60%">
         <link href="../../../public/css/table1.css" rel="stylesheet" />
            
             <thead>
                 <tr>
                    
-                    <th>Order ID</th>
+                    
                     <th>Delivery Status</th>
                     <th>Additional Notes</th>
                     
                 </tr>
             </thead>
             <tbody>
-            
+            <?php while($row = mysqli_fetch_array($query_run)){
+    }?>
             <tr>
-            <td><input type="num" placeholder="Enter Order ID" name="order_id" >&nbsp;<input type="submit" class="link" name="search" style="margin-bottom: 50px; width:15%; height:40px;background-color:  rgb(236, 185, 17);" value="Search"> </td>
-           
-        <td> <select name="delivery_status">
+            
+        <td> <select name="delivery_status" value= <?php echo $row['delivery_status']?>>
             <option value="yes">yes</option>
             <option value="no">no</option>
             <option value="in_transit">in_transit</option>
             </select> </td>
-            <td><input type="text" placeholder="Add addtional notes" name="additional_notes" > </td>
-            
+            <td><input type="text" placeholder="Add addtional notes" name="additional_notes" value="<?php echo $row['additional_notes'] ?>"
+             > </td>
+        
             </tbody>
         </table>
         <br>
-        <br><center><input type="submit" class="link" name="update" style="margin-bottom: 50px; width:15%; height:40px;background-color:  rgb(236, 185, 17);" value="Update"></center>
-        </div>
+        <br><center><input type="submit" class="link" name="Update" style="margin-bottom: 50px; width:15%; height:40px;background-color:  rgb(236, 185, 17);" value="Update"></center>
+        
         </form>
-        </div>
+        
     </body>
    
 </html>
