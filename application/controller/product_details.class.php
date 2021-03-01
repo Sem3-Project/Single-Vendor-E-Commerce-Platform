18682d2498a500fb4bdd29ac4804660f6579316d
Adeesha Wijesinghe <63795833+anw98@users.noreply.github.com>
2021-03-01 18:08:46 +0530

    Update product_details.class.php

--- application/controller/product_details.class.php
+++ application/controller/product_details.class.php
@@ -7,7 +7,7 @@ class ProductDetails{
           
         // make the connection with database  
         $connector = new DbConnection();
-        $conn = $connector->connect();  
+        $conn = $connector->connect1();  
     }  
     function __destruct() {  
           
