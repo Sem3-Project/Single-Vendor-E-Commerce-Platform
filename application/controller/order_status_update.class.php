d97f2a7956b75db9261b4e2998544b72acb0dc33
Adeesha Wijesinghe <63795833+anw98@users.noreply.github.com>
2021-03-02 10:08:25 +0530

    Update order_status_update.class.php

--- application/controller/order_status_update.class.php
+++ application/controller/order_status_update.class.php
@@ -12,6 +12,20 @@ class UpdateStatus{
     function __destruct() {  
           
     } 
+    public function getestiDate($conn,$order_id){
+        $date = mysqli_query($conn,"SELECT `date` from estimate_delivery where order_id='".$order_id."'");
+        $city = mysqli_query($conn,"SELECT city from estimate_delivery where order_id='".$order_id."'");
+        
+        
+        if ($city=='Colombo'){
+            $result= $date->modify('+5 day');
+        }
+        else{
+            $result= $date->modify('+7 day');
+        }
+        $result = $delivery_estimate->fetch_assoc();
+        return $result['delivery_estimate'];
+    }
     // public function loadOrder($conn,$order_id){
     //     $loadQr = mysqli_query($conn,"SELECT delivery_status,additional_notes FROM delivery WHERE order_id = '".$order_id."'");  
     //     return $loadQr;
