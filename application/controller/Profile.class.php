f019c713cf21c63a509c1110db4e8fb4274047c5
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-03-03 20:34:36 +0530

    MOD - visa card encrypt and decrypt

--- application/controller/Profile.class.php
+++ application/controller/Profile.class.php
@@ -1,37 +1,55 @@
 <?php
 include 'controllerUserData.php';
 
-class Profile{  
-            
-    // function __construct() {  
-          
-    //     // make the connection with database  
-    //     $connector = new DbConnection();
-    //     $conn = $connector->connect1();  
-    // }  
-    // function __destruct() {  
-          
-    // } 
-    
-    public function editProfile($con1,$customer_id,$email,$payment_number,$first_name,$last_name,$zip_code,$address_line_1,$address_line_2,$city,$state,$mobile_num){  
-        $updateQr = mysqli_query($con1,"UPDATE customer SET email='".$email."',payment_number='".$payment_number."',first_name='".$first_name."',last_name='".$last_name."',zip_code='".$zip_code."',address_line_1='".$address_line_1."',address_line_2='".$address_line_2."',city='".$city."',state='".$state."',mobile_num=$mobile_num WHERE customer_id = '".$customer_id."'");  
-        
-       //$updateQr = mysqli_query($conn,"UPDATE customer SET email=$email,payment_number=$payment_number,first_name=$first_name,last_name=$last_name,zip_code=$zip_code,address_line_1=$address_line_1,address_line_2=$address_line_2,city=$city,state=$state,mobile_num=$mobile_num WHERE customer_id ='".$customer_id."'");  
+class Profile
+{
 
-      // $updateQr = mysqli_query($conn,"UPDATE customer SET email=$email,payment_number=$payment_number,first_name=$first_name,last_name=$last_name,zip_code=$zip_code WHERE customer_id ='".$customer_id."'");  
+  // function __construct() {  
 
-        return $updateQr;
-    }
+  //     // make the connection with database  
+  //     $connector = new DbConnection();
+  //     $conn = $connector->connect1();  
+  // }  
+  // function __destruct() {  
 
-    public function loadProfile($con1,$customer_id){
-        $loadQr = mysqli_query($con1,"SELECT customer_id,email,payment_number,first_name,last_name,zip_code,address_line_1,address_line_2,city,state,mobile_num FROM customer WHERE customer_id = '".$customer_id."'");  
-        return $loadQr;
-    }
+  // } 
+
+  public function editProfile($con1, $customer_id, $email, $payment_number, $first_name, $last_name, $zip_code, $address_line_1, $address_line_2, $city, $state, $mobile_num)
+  {
+    $ciphering = "AES-128-CTR";
+
+    // Use OpenSSl Encryption method 
+    $iv_length = openssl_cipher_iv_length($ciphering);
+    $options = 0;
+
+    // Non-NULL Initialization Vector for encryption 
+    $encryption_iv = '1234567891011121';
+
+    // Store the encryption key 
+    $encryption_key = "CSmartEcomPlatform";
+
+    // Use openssl_encrypt() function to encrypt the data 
+    $encryption = openssl_encrypt($payment_number, $ciphering, $encryption_key, $options, $encryption_iv);
+    $updateQr = mysqli_query($con1, "UPDATE customer SET email='" . $email . "',payment_number='" . $encryption . "',first_name='" . $first_name . "',last_name='" . $last_name . "',zip_code='" . $zip_code . "',address_line_1='" . $address_line_1 . "',address_line_2='" . $address_line_2 . "',city='" . $city . "',state='" . $state . "',mobile_num=$mobile_num WHERE customer_id = '" . $customer_id . "'");
+
+    //  $updateQr = mysqli_query($con1,"UPDATE customer SET email='".$email."',payment_number='".$payment_number."',first_name='".$first_name."',last_name='".$last_name."',zip_code='".$zip_code."',address_line_1='".$address_line_1."',address_line_2='".$address_line_2."',city='".$city."',state='".$state."',mobile_num=$mobile_num WHERE customer_id = '".$customer_id."'");  
+
+    //$updateQr = mysqli_query($conn,"UPDATE customer SET email=$email,payment_number=$payment_number,first_name=$first_name,last_name=$last_name,zip_code=$zip_code,address_line_1=$address_line_1,address_line_2=$address_line_2,city=$city,state=$state,mobile_num=$mobile_num WHERE customer_id ='".$customer_id."'");  
+
+    // $updateQr = mysqli_query($conn,"UPDATE customer SET email=$email,payment_number=$payment_number,first_name=$first_name,last_name=$last_name,zip_code=$zip_code WHERE customer_id ='".$customer_id."'");  
+
+    return $updateQr;
+  }
+
+  public function loadProfile($con1, $customer_id)
+  {
+    $loadQr = mysqli_query($con1, "SELECT customer_id,email,payment_number,first_name,last_name,zip_code,address_line_1,address_line_2,city,state,mobile_num FROM customer WHERE customer_id = '" . $customer_id . "'");
+    return $loadQr;
+  }
   //  public function editProfile($conn,$customer_id,$email,$payment_number,$first_name,$last_name,$zip_code,$address_line_1,$address_line_2,$city,$state,$mobile_num){  
 
-    // public function updateproflile($conn,$customer_id,$address_line_1){
-    //     $updateq = mysqli_query($conn,"UPDATE customer SET address_line_1='".$address_line_1."' WHERE customer_id=$customer_id");
-    // return $updateq;
-    // }
-}  
-?>  
+  // public function updateproflile($conn,$customer_id,$address_line_1){
+  //     $updateq = mysqli_query($conn,"UPDATE customer SET address_line_1='".$address_line_1."' WHERE customer_id=$customer_id");
+  // return $updateq;
+  // }
+}
--- application/model/account/account.model.php
+++ application/model/account/account.model.php
@@ -5,47 +5,64 @@ include '../../controller/Profile.class.php';
 // $conn = $connector->connect1();
 $funObj = new Profile();
 
-$load = $funObj->loadProfile($con1,$_SESSION['customer_id']); ////set session to get customer id==========================
+$load = $funObj->loadProfile($con1, $_SESSION['customer_id']); ////set session to get customer id==========================
 
-if (isset($_POST['update'])){
-    $update = $funObj->editProfile($con1,$_SESSION['customer_id'],$_POST['email'],$_POST['payment_number'],$_POST['first_name'],$_POST['last_name'],$_POST['zip_code'],$_POST['address_line_1'],$_POST['address_line_2'],$_POST['city'],$_POST['state'],$_POST['mobile_num']);
+if (isset($_POST['update'])) {
+    $update = $funObj->editProfile($con1, $_SESSION['customer_id'], $_POST['email'], $_POST['payment_number'], $_POST['first_name'], $_POST['last_name'], $_POST['zip_code'], $_POST['address_line_1'], $_POST['address_line_2'], $_POST['city'], $_POST['state'], $_POST['mobile_num']);
     header("Location:account.model.php");
 }
-if ($load){
-    if(mysqli_num_rows($load) > 0){
-        while($row = mysqli_fetch_array($load)){
-            $customer_id = $row ['customer_id'];
-            $email = $row ['email'];
-            $first_name = $row ['first_name'];
-          //  $dispnum = substr($row['cardnumber'], 0, 2) . str_repeat("*", strlen($row['cardnumber'])-2);
+if ($load) {
+    if (mysqli_num_rows($load) > 0) {
+        while ($row = mysqli_fetch_array($load)) {
+            $customer_id = $row['customer_id'];
+            $email = $row['email'];
+            $first_name = $row['first_name'];
+            //  $dispnum = substr($row['cardnumber'], 0, 2) . str_repeat("*", strlen($row['cardnumber'])-2);
 
             //$payment_number = $row ['payment_number'];
-            $last_name = $row ['last_name'];
-            $zip_code = $row ['zip_code'];
-            $address_line_1 = $row ['address_line_1'];
-            $address_line_2 = $row ['address_line_2'];
-            $city = $row ['city'];
-            $state = $row ['state'];
-            $mobile_num = $row ['mobile_num'];
-            $payment_num = $row ['payment_number'];
-            if ($row['payment_number']!=""){
-                $payment_number = substr($row['payment_number'], 0, 4) . str_repeat("*", strlen($row['payment_number'])-4);
+            $last_name = $row['last_name'];
+            $zip_code = $row['zip_code'];
+            $address_line_1 = $row['address_line_1'];
+            $address_line_2 = $row['address_line_2'];
+            $city = $row['city'];
+            $state = $row['state'];
+            $mobile_num = $row['mobile_num'];
+            $payment_num = $row['payment_number'];
 
-            }
-            else{
-                $payment_number = $row['payment_number'];
-            }
+            // Non-NULL Initialization Vector for decryption 
+            $decryption_iv = '1234567891011121';
+            $ciphering = "AES-128-CTR";
+            // Store the decryption key 
+            $decryption_key = "CSmartEcomPlatform";
+            $options = 0;
+            // Use openssl_decrypt() function to decrypt the data 
+            $decryption = openssl_decrypt(
+                $payment_num,
+                $ciphering,
+                $decryption_key,
+                $options,
+                $decryption_iv
+            );
 
-            
+            // if ($row['payment_number'] != "") {
+            //     $payment_number = substr($row['payment_number'], 0, 4) . str_repeat("*", strlen($row['payment_number']) - 4);
+            // } else {
+            //     $payment_number = $row['payment_number'];
+            // }
+            if ($decryption != "") {
+                $payment_number = substr($decryption, 0, 4) . str_repeat("*", strlen($decryption) - 4);
+            } else {
+                $payment_number = $decryption;
+            }
         }
     }
     // if (isset($_POST['update'])){
     //     $update = $funObj->editProfile($conn,$_POST['customer_id'],$_POST['email'],$_POST['payment_number'],$_POST['first_name'],$_POST['last_name'],$_POST['zip_code'],$_POST['address_line_1'],$_POST['address_line_2'],$_POST['city'],$_POST['state'],$_POST['mobile_num']);
-    
+
     //     //$update = $funObj->updateproflile($conn,14,$_POST['address_line_1']);
     //     // $update = $funObj->editProfile($conn,15,$_POST['email'],$_POST['payment_number'],$_POST['first_name'],$_POST['last_name'],$_POST['zip_code']);
     // }
-    
+
 }
 
 
